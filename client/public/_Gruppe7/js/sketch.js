9398aff691e2cf2d6f8ab039b40f22f66840e00d
weitzelc <katja16j@hotmail.fr>
2021-03-16 15:37:38 +0100

    Update sketch.js
    Hartmuts änderungen

--- client/public/_Gruppe7/js/sketch.js
+++ client/public/_Gruppe7/js/sketch.js
@@ -5,39 +5,53 @@ socket.on('connected', function (msg) {
 });
 
 // Sending a userID will help to know if the message came from me or from others
-let myUserID = Math.random().toString(36).substr(2, 9).toUpperCase();
+
 
 
 // Your script starts here ------------------------------------------------------
 
+let myUserIndex = 1;
+
+let lastX = [0, 0, 0, 0];
+let lastY = [0, 0, 0, 0];
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
     fill(255, 128, 0);
+    
 }
 
 function draw() {
     // put drawing code here
+    background(255);
+       
+    for (let i = 0; i < lastX.length; i++) {
+        rect(lastX[i], lastY[i], 40, 20);
+    }
+
 }
 
-function mouseDragged() {
+function mouseMoved() {
     //console.log(mouseX, mouseY);
 
     // Sending an event 
-    socket.emit('serverEvent', myUserID, mouseX, mouseY);
+    socket.emit('serverEvent', myUserIndex, mouseX, mouseY);
 }
 
 // Incoming events 
-socket.on('serverEvent', function (user, x, y) {
+socket.on('serverEvent', function (userIndex, x, y)  
+{
     //console.log("Incoming event: ", user, x, y);
     
-    if (user == myUserID) { 
-      fill(128, 80);
-    } else {
-      fill(255, 128, 0, 100);
-    }
+    // if (user == myUserID) { 
+    //   fill(128, 100);
+    // } 
+    // else {
+    //   fill(255, 128, 0, 100);
+    // }
 
-    circle(x, y, 20);
+    lastX[0] = x;
+    lastY[0] = y;
 });
 
