d107ee41b457537026ed9304558d5f5def9f2ede
bunlisa <75975780+bunlisa@users.noreply.github.com>
2021-03-17 12:08:33 +0100

    Merge branch 'main' of https://github.com/bunlisa/2121-ig2-grundlagen-im-medialen-raum into main
    xlbkm

--- client/public/_Gruppe7/js/sketch.js
+++ client/public/_Gruppe7/js/sketch.js
@@ -9,11 +9,16 @@ socket.on('connected', function (msg) {
 
 // Your script starts here ------------------------------------------------------
 
-let myUserIndex = Math.random().toString(36).substr(2, 9).toUpperCase();
-// let playerColors = ['#f80', '#08f', '#80f', '#0f8', '#8f0', '#f08']
-let userCount = 0;
-// let whosTurn = 0;
+Matter.use('matter-wrap');
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Body = Matter.Body;
+const Bodies = Matter.Bodies;
 
+let myPlayerIndex = Math.random().toString(36).substr(2, 9).toUpperCase();
+
+let playerCount = 0;
 
 
 let lastX = [0, 40, 80, 120];
@@ -21,16 +26,35 @@ let lastY = [0, 0, 0, 0];
 
 let colors = ['purple', 'blue', 'yellow','green']
 
+let engine;
+let circle;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     noStroke();
-    //fill(255, 128, 0);
     
+    engine = Engine.create();
+
+    circle = Bodies.circle(width/2, 50, 40, {
+      restitution: 0
+    });
+    circle.plugin.wrap = {
+      min: { x: 0, y: 0 },
+      max: { x: width, y: height }
+    };
+
+    World.add(engine.world, [circle]);
+
+  Engine.run(engine);
 }
 
 function draw() {
     // put drawing code here
     background(255);
+
+    noStroke();
+  fill(255);
+  drawVertices(circle.vertices);
        
     for (let i = 0; i < lastX.length; i++) {
        fill(colors[i]);
@@ -45,30 +69,36 @@ function mouseMoved() {
 
     // Sending an event 
     socket.emit('serverEvent', myUserIndex, mouseX, mouseY);
+
+    // updateStatus();
+
 }
+
+function drawVertices(vertices) {
+    beginShape();
+    for (let i = 0; i < vertices.length; i++) {
+      vertex(vertices[i].x, vertices[i].y);
+    }
+    endShape(CLOSE);
+  }
+
+//function updateStatus() {
+//   $('#player-status').html("There are " + playerCount + " players connected");
+//    $('#playcolor').css("background-color", playerColors[myPlayerIndex]);
+//    $('body').css("background-color", playerColors[myUserIndex]+"4"); // background color like playing color but less opacity
+
+//}
 
 // Incoming events 
 socket.on('serverEvent', function (index, x, y)  
 {
-    //console.log("Incoming event: ", user, x, y);
-    
-    // if (user == myUserID) { 
-    //   fill(128, 100);
-    // } 
-    // else {
-    //   fill(255, 128, 0, 100);
-    // }
 
+// ein Spieler, ein Balken
     lastX[index] = x;
     lastY[index] = y;
 });
 
 socket.on('newUsersEvent', function (myID, myIndex, userList) {
-    // console.log("New users event: ");
-    // console.log("That's me: " + myID);
-    // console.log("My index in the list: " + myIndex);
-    // console.log("That's the new users: ");
-    // console.log(userList);
 
     playerCount = userList.length;
     myUserIndex = myIndex;
