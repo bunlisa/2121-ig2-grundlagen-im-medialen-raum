24e908971b113b8f8bb0b78a2df3ee091ef9ba96
weitzelc <katja16j@hotmail.fr>
2021-03-20 09:25:54 +0100

    farbige boards

--- client/public/_Gruppe7/js/sketch.js
+++ client/public/_Gruppe7/js/sketch.js
@@ -21,9 +21,13 @@ let myPlayerIndex = Math.random().toString(36).substr(2, 9).toUpperCase();
 let engine;
 let circle;
 
-let blocks = []
-let board = []
+let blocks4 = []
+let blocks1 = []
+let blocks2 = []
+let blocks3 = []
 let wände = []
+let mauern = []
+let walls = []
 
 let boardConstraints =[]
 
@@ -65,32 +69,34 @@ wände.push(Bodies.rectangle(529, 652, 48, 45, {isStatic: true}));
 wände.push(Bodies.rectangle(473, 52, 169, 50, {isStatic: true}));
 
 //obere Balken
-wände.push(Bodies.rectangle(604, 52, 23, 107, {isStatic: true}));
-wände.push(Bodies.rectangle(733, 232, 23, 463, {isStatic: true}));
-wände.push(Bodies.rectangle(862, 177, 23, 356, {isStatic: true}));
+mauern.push(Bodies.rectangle(604, 52, 23, 107, {isStatic: true}));
+mauern.push(Bodies.rectangle(733, 232, 23, 463, {isStatic: true}));
+mauern.push(Bodies.rectangle(862, 177, 23, 356, {isStatic: true}));
 
 // //untere Balken
-wände.push(Bodies.rectangle(604, 490, 23, 592, {isStatic: true}));
-wände.push(Bodies.rectangle(733, 678, 23, 215, {isStatic: true}));
-wände.push(Bodies.rectangle(861, 619, 23, 332, {isStatic: true}));
+mauern.push(Bodies.rectangle(604, 490, 23, 592, {isStatic: true}));
+mauern.push(Bodies.rectangle(733, 678, 23, 215, {isStatic: true}));
+mauern.push(Bodies.rectangle(861, 619, 23, 332, {isStatic: true}));
 
 // //3.Stufe
-wände.push(Bodies.rectangle(1355, 116, 176, 52, {isStatic: true}));
-wände.push(Bodies.rectangle(1400, 240, 75, 196, {isStatic: true}));
-wände.push(Bodies.rectangle(1225, 769, 436, 52, {isStatic: true}));
-wände.push(Bodies.rectangle(944, 341, 113, 17, {isStatic: true, angle: Math.PI * 0.06}));
-wände.push(Bodies.rectangle(944, 654, 113, 17, {isStatic: true, angle: Math.PI * 0.06, restitution: 0.8}));
-wände.push(Bodies.rectangle(1278, 521, 86, 17, {isStatic: true, angle: Math.PI * 0.06}));
-wände.push(Bodies.rectangle(1167, 588, 113, 17, {isStatic: true, angle: Math.PI * -0.06}));
-wände.push(Bodies.rectangle(1399, 540, 98, 17, {isStatic: true, angle: Math.PI * -0.15}));
+walls.push(Bodies.rectangle(1355, 116, 176, 52, {isStatic: true}));
+walls.push(Bodies.rectangle(1400, 240, 75, 196, {isStatic: true}));
+walls.push(Bodies.rectangle(1225, 769, 436, 52, {isStatic: true}));
+walls.push(Bodies.rectangle(944, 341, 113, 17, {isStatic: true, angle: Math.PI * 0.06}));
+walls.push(Bodies.rectangle(944, 654, 113, 17, {isStatic: true, angle: Math.PI * 0.06, restitution: 0.8}));
+walls.push(Bodies.rectangle(1278, 521, 86, 17, {isStatic: true, angle: Math.PI * 0.06}));
+walls.push(Bodies.rectangle(1167, 588, 113, 17, {isStatic: true, angle: Math.PI * -0.06}));
+walls.push(Bodies.rectangle(1399, 540, 98, 17, {isStatic: true, angle: Math.PI * -0.15}));
 
 wände.push(Bodies.rectangle(721, 830, 1443, 108, {isStatic: true}));
 
 World.add(engine.world, wände)
+World.add(engine.world, mauern)
+World.add(engine.world, walls)
 
 //Board 1   
     board1 = Bodies.rectangle(50, 180, 100, 30, {color: 'blue'});
-    blocks.push(board1);
+    blocks1.push(board1);
 
     constraint1 = Constraint.create({
       pointA: { x: 25, y: 180 },
@@ -111,7 +117,7 @@ World.add(engine.world, wände)
 
 //Board 2
     board2 = Bodies.rectangle(50, 230, 100, 30, {color: 'green'});
-    blocks.push(board2);
+    blocks2.push(board2);
 
     constraint3 = Constraint.create({
       pointA: { x: 25, y: 230 },
@@ -134,7 +140,7 @@ World.add(engine.world, wände)
 
 //Board 3
     board3 = Bodies.rectangle(50, 280, 100, 30, {color: 'red'});
-    blocks.push(board3);
+    blocks3.push(board3);
 
       constraint5 = Constraint.create({
         pointA: { x: 25, y: 280 },
@@ -154,7 +160,7 @@ World.add(engine.world, wände)
 
 //Board 4
       board4 = Bodies.rectangle(50, 330, 100, 30, {color: 'orange'});
-      blocks.push(board4);
+      blocks4.push(board4);
 
        constraint7 = Constraint.create({
         pointA: { x: 25, y: 330 },
@@ -170,7 +176,6 @@ World.add(engine.world, wände)
       });
       boardConstraints.push([constraint7, constraint8]);
       World.add(engine.world, [board4, constraint7, constraint8]);
-    Matter.World.add(engine.world, board)
   
 
   Engine.run(engine);
@@ -183,11 +188,21 @@ function draw() {
     
     drawVertices(circle.vertices);
 
-    // fill(this.color);
-    blocks.forEach(board => drawBody(board))
+    fill(255, 200, 250);    
+    blocks1.forEach(board => drawBody(board))
+    fill(200, 0, 250);    
+    blocks2.forEach(board => drawBody(board))
+    fill(255, 200, 0);    
+    blocks3.forEach(board => drawBody(board))
+    fill(255, 0, 0);    
+    blocks4.forEach(board => drawBody(board))
 
     fill(0, 200, 250);
     wände.forEach(board => drawBody(board))
+    fill(0, 0, 250);
+    mauern.forEach(board => drawBody(board))
+    fill(0, 200, 0);
+    walls.forEach(board => drawBody(board))
 }
 
 function drawBody(body) {
